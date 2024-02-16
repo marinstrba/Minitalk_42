c917bc313facf8f525de5bd7139b3bdded86946a
Marian Strba <marinstrba@gmail.com>
2024-02-16 17:06:46 +0100

    another version.

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mstrba <mstrba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:40:57 by mstrba            #+#    #+#             */
-/*   Updated: 2024/02/16 15:46:39 by mstrba           ###   ########.fr       */
+/*   Updated: 2024/02/16 17:03:54 by mstrba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include "../lib/libft/libft.h"
 
-
-int	char_to_bin(char c, int pid);
+void	args_checker(int argc, char **argv);
+void	send_message(__pid_t pid, char	*message);
 
 int	main(int argc, char	**argv)
 {
-	int					server_id;
-	char				*message;
-	struct sigaction 	sa;
+	__pid_t	server_pid;
 
-	if (argc != 3)
-	{
-		printf("Wrong number of arguments.\n");
-		exit(-1);
-	}
-	else
-	{
-		
-	}
+	args_checker(argc, argv);
+	server_pid = atoi(argv[1]);
+	send_message(server_pid, argv[1]);
+
 	return (0);
 }
 
-int	char_to_bin(char c, int pid)
+void	args_checker(int argc, char **argv)
 {
-	
+	int	index;
+
+	index = 0;
+	if (argc != 3)
+		error_handler("Invalid number of arguments.\n");
+	while (argv[1][index])
+		if (!ft_isdigit(argv[1][index++]))
+			error_handler("Invalid server id.");
+	if (*argv[2] == 0)
+		error_handler("The message is empty.");
+}
+
+void	send_message(__pid_t pid, char	*message)
+{
+	unsigned char	c;
+
+	while (*message)
+	{
+		c = *message;
+	}
+
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mstrba <mstrba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:42:50 by mstrba            #+#    #+#             */
-/*   Updated: 2024/02/16 15:38:12 by mstrba           ###   ########.fr       */
+/*   Updated: 2024/02/16 16:45:15 by mstrba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include "../lib/minitalk/extra.h"
 
 void	signal_handle_client(int sig);
+void	init_signals(void);
 
 int	main(int argc, char	**argv)
 {
-	struct sigaction	sa;
+	__pid_t	pid;
 
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = signal_handle_client;
-
-	printf("This is the id of the server: %d\n", getpid());
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-	{
-		perror("Error setting SIGUSR1 handler.");
-		exit (EXIT_FAILURE);
-	}
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
-		perror("Error setting SIGUSR2 handler.");
-		exit (EXIT_FAILURE);
-	}
+	pid = getpid();
+	printf("This is the id of the server: %d\n", pid);
+	init_signals();
 	while (1)
 		pause();
 	return (0);
@@ -43,5 +33,25 @@ int	main(int argc, char	**argv)
 
 void	signal_handle_client(int sig)
 {
-	
+
+}
+
+void	init_signals(void)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = signal_handle_client;
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		error_handler("Error setting SIGUSR1 handler.\n");
+		exit (EXIT_FAILURE);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		error_handler("Error setting SIGUSR2 handler.\n");
+		exit (EXIT_FAILURE);
+	}
 }
